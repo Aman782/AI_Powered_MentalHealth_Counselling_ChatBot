d4a3c6835e82b822fdace002ad3a73e5db37358f
Aman Pandey <amanpandey45692@gmail.com>
2024-12-05 02:48:10 +0530

    Hero component done

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className='container-fuild p-5 border'>
+        <div className='row d-flex align-items-center justify-content-evenly'>
+            <div className='col-md-5'>
+                <p className='fs-2 text-center fw-semibold'><span className='textStyle'>Simple & Effective Solutions</span>  <br></br> for your mental health & well-being.</p>
+                <p>No matter where you are in life, you are never too far gone to make positive changes. Your mental health is the foundation of your happiness and success, and seeking support is the first step in building a more resilient and fulfilling life</p>
+
+                <button className='btn btn-success btn-lg'>
+                   Get Started
+                </button>
+            </div>
+
+            <div className='col-md-4'>
+               <img src='./images/brain.svg'></img>
+            </div>
+        </div>
+      </div>
+    </>
   )
 }
 
