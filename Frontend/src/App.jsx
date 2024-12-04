808310195c18c4517ca4468eeb21682ad86aba12
Aman Pandey <amanpandey45692@gmail.com>
2024-12-05 02:24:09 +0530

    navbar done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Home from './components/Home'
 
 function App() {
 
 
   return (
     <>
-    <h1>Hello!</h1>
+    <Home />
     </>
   )
 }
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+     <>
+      <div className="navbar d-flex p-3 border">
+        <ul>
+          <a className="fs-5">Home</a>
+          <a className="fs-5">About</a>
+          <a className="fs-5">Services</a>
+          <a className="fs-5">Contact</a>
+        </ul>
+        
+        <div className="auth ms-auto">
+        <button className="btn btn-primary fs-5 mx-2">
+           Signup
+        </button>
+
+        <button className="btn btn-outline fs-5 mx-2">
+           Login
+        </button>
+         </div>
+        
+      </div>
+     </>
+  );
 }
 
-export default Navbar
+export default Navbar;
